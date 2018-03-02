10b3f90fd749dd3ee50b3eaba2c2b61e4b20b6ca
Dennis Jessen <digidennis3@gmail.com>
2018-03-02 10:56:18 +0000

    adminhtml

--- code/Block/Adminhtml/Dimension/Info/Global.php
+++ code/Block/Adminhtml/Dimension/Info/Global.php
@@ -7,6 +7,9 @@
  */
 class Digidennis_DimensionIt_Block_Adminhtml_Dimension_Info_Global extends Mage_Adminhtml_Block_Template
 {
+
+    private $_foundoption;
+
     /**
      * Get order item object from parent block
      *
@@ -15,6 +18,30 @@ class Digidennis_DimensionIt_Block_Adminhtml_Dimension_Info_Global extends Mage_
     public function getItem()
     {
         return $this->getParentBlock()->getData('item');
+    }
+
+    public function hasDimensionOutput()
+    {
+        $item = $this->getItem();
+        $addoptions = $item->getOptionByCode('additional_options');
+        if($addoptions){
+            $addoptions = unserialize($addoptions->getValue());
+            foreach ($addoptions as $option ){
+                if($option['code'] == 'dimension_total'){
+                    $this->_foundoption = $option;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function getDimensionOutputHtml()
+    {
+        if($this->_foundoption){
+            return "<label>{$this->_foundoption['label']}:</label> {$this->_foundoption['value']}";
+        }
+        return '';
     }
 
 }
